810e7c2bc11b0490d33a2b0776f45db63beab2ee
7c <774669939@qq.com>
2018-10-18 17:15:42 +0800

    栅格拖拽及小组件拖拽

--- src/js/drag.js
+++ src/js/drag.js
@@ -1,5 +1,7 @@
 window.onload = function () {
-    $('#portalmask').hide();
+    // setTimeout(function () {
+    $('#portalmask').hide(700);
+    // }, 500);
 };
 $(function () {
     changeFrameHeight();
@@ -14,10 +16,7 @@ $(function () {
         axis: "y",
         handle: "h3",
         stop: function (event, ui) {
-            // IE doesn't register the blur when sorting
-            // so trigger focusout handlers to remove .ui-state-focus
             ui.item.children("h3").triggerHandler("focusout");
-            // Refresh accordion to handle new order
             $(this).accordion("refresh");
         }
     });
@@ -25,7 +24,7 @@ $(function () {
 
 function changeFrameHeight() {
     var box = parent.$('.dragbox');
-    $('.dragbox').css({"min-height": $(window).height() - box[0].getBoundingClientRect().top - 10})
+    $('.dragbox').css({"min-height": $(window).height() - box[0].getBoundingClientRect().top - 10});
 }
 
 $('.pagetitle').click(function () {
@@ -33,30 +32,12 @@ $('.pagetitle').click(function () {
         $('.typelist').removeClass('sidebar-close');
         $('.drag-container').css("margin-left", "400px");
         $(".main-content").css("margin-left", "");
-        $('.typelist').show();
+        $('.typelist').show(100);
     } else {
         $('.typelist').addClass('sidebar-close');
         $('.drag-container').css("margin-left", "0px");
         $('.main-content').css("margin-left", "0px");
-        $('.typelist').hide();
-    }
-});
-
-$('#colsplit').bind('input propertychange', function () {
-    var checknum = 0;
-    var content = "";
-    var colsplit = $('#colsplit').val();
-    var arr = colsplit.trim().split(" ");
-    for (var i = 0; i < arr.length; i++) {
-        checknum += parseInt(arr[i]);
-    }
-    if (checknum == 12) {
-        // $('#colsplitbtn').removeAttr("disabled");
-        $(".components-col .col-layout").children('*[class^="col"]').remove();
-        for (var i = 0; i < arr.length; i++) {
-            content += '<div class="col-sm-' + arr[i] + '" style="border :1px solid #999;min-height:65px;"></div>';
-        }
-        $(".components-col .col-layout").append(content);
+        $('.typelist').hide(100);
     }
 });
 
@@ -104,3 +85,97 @@ $('#downbtn').click(function () {
     });
 });
 
+$('#colsplit').bind('input propertychange', function () {
+    var checknum = 0;
+    var content = "";
+    var colsplit = $('#colsplit').val();
+    var arr = colsplit.trim().split(" ");
+    for (var i = 0; i < arr.length; i++) {
+        checknum += parseInt(arr[i]);
+    }
+    if (checknum == 12) {
+        $('.component-layout .draglabel').removeAttr("disabled");
+        $(".components-col .col-layout").children('*[class^="col"]').remove();
+        for (var i = 0; i < arr.length; i++) {
+            content += '<div class="col-sm-' + arr[i] + '" style="border :1px solid #999;min-height:65px;"></div>';
+        }
+        $(".components-col .col-layout").append(content);
+        $(".drag-component.component-layout").draggable({
+            handle: ".draglabel",
+            appendTo: "body",
+            helper: "clone"
+        });
+        dragboxdrag();
+    }
+});
+
+var dragboxdrag = function () {
+    $(".dragbox").droppable({
+        accept: ".drag-component.component-layout",
+        helper: "clone",
+        hoverClass: "droppable-active",
+        drop: function (event, ui) {
+            var uidraggable = $(ui.draggable);
+            if (!uidraggable.hasClass("dropped")) {
+                uidraggable.clone().addClass("dropped").css({
+                    "position": "static",
+                    "left": null,
+                    "right": null
+                }).appendTo(this);
+                // if ($el.children()[1] == null || $el.children()[1].className == "row col-layout") {
+                //     $('<button class="btn btn-danger btn-xs remove-link"><i class="fa fa-trash"></i>移除</button>').appendTo($el.children()[0]);
+                // }
+            } else {
+                if ($(this)[0] != uidraggable.parent()[0]) {
+                    uidraggable.clone().css({
+                        "position": "static",
+                        "left": null,
+                        "right": null
+                    }).appendTo(this);
+                    uidraggable.remove();
+                }
+            }
+            $('.drag-container .hiddentoolbar').show();
+            $(".drag-component.component-button").draggable({
+                handle: ".draglabel",
+                appendTo: "body",
+                helper: "clone"
+            });
+            colcomponentdarg();
+        }
+    }).sortable();
+};
+
+var colcomponentdarg = function () {
+    $("[class*=col-sm-]").droppable({
+        accept: ".drag-component.component-button",
+        helper: "clone",
+        greedy: true,
+        hoverClass: "droppable-active",
+        drop: function (event, ui) {
+            var uidraggable = $(ui.draggable);
+            if (!uidraggable.hasClass("dropped")) {
+                uidraggable.clone().addClass("dropped").css({
+                    "position": "static",
+                    "left": null,
+                    "right": null,
+                    "width": "auto"
+                }).appendTo(this);
+            } else {
+                if ($(this)[0] != uidraggable.parent()[0]) {
+                    uidraggable.clone().css({
+                        "position": "static",
+                        "left": null,
+                        "right": null
+                    }).appendTo(this);
+                    uidraggable.remove();
+                }
+            }
+            $('.drag-container .hiddentoolbar').show();
+        }
+    }).sortable();
+}
+
+$(document).on("click", ".remove-link", function () {
+    $(this).parent().parent().parent().remove();
+});
