9867391b611d9fd4f74840855209816488fd009f
7c <774669939@qq.com>
2018-10-29 15:10:57 +0800

    gulp后给gitee使用

--- gulpfile.js
+++ gulpfile.js
@@ -17,4 +17,38 @@ gulp.task('watchserver',function(){
 		]).on('change',reload);
 });
 
+gulp.task('css-min',  function() {
+    return gulp.src(['src/css/*.css'],{base:'src'})
+        .pipe(minifyCss())
+        .pipe(gulp.dest('G:\\WebstormProjects\\drag-gitee\\src'));
+});
+
+gulp.task('js-min',  function() {
+    return gulp.src(['src/js/*.js'],{base:'src'})
+        .pipe(uglify())
+        .pipe(gulp.dest('G:\\WebstormProjects\\drag-gitee\\src'));
+});
+
+// gulp.task('img-min',  function() {
+//     return gulp.src(['src/img/*/*'],{base:'src'})
+//         .pipe(imagemin())
+//         .pipe(gulp.dest('src-min'));
+// });
+
+gulp.task('html-min', function () {
+    var options = {
+        removeComments: true,
+        collapseWhitespace: true,
+        collapseBooleanAttributes: true,
+        removeEmptyAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        minifyJS: true,
+        minifyCSS: true
+    };
+    return gulp.src(['build/*.html'],{base:'build'})
+        .pipe(jspmin(options))
+        .pipe(gulp.dest('G:\\WebstormProjects\\drag-gitee\\build'));
+});
 gulp.task('reloadpage',['watchserver']);
+gulp.task('gitee',['css-min','js-min','html-min']);
